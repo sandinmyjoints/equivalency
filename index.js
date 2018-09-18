59490acfeb6185f70d827dd1342f4b8c9535558f
William Bert <william@spanishdict.com>
2018-09-18 16:17:39 -0700

    Implement doesntMatter.

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
+const assert = require('assert');
+const isString = require('is-string');
+
+function isMap(obj) {
+  return obj instanceof Map;
+}
+
+function mapFromString(s) {
+  const iterable = Array.from(s).map(c => [c, '']);
+  return new Map(iterable);
+}
+
 class Rubric {
   constructor() {
     this.rules = [];
     this.finalMap = null;
   }
 
-  // doesntMatter(rule) {}
+  doesntMatter(rule) {
+    let map;
+    if (isString(rule)) {
+      map = mapFromString(rule);
+    } else if (isMap(rule)) {
+      map = rule;
+    } else {
+      throw new Error('need string or Map');
+    }
 
-  // matters(rule) {
-  //   this.rules.push(rule);
-  // }
+    this.rules.push(map);
+
+    return this;
+  }
+
+  matters() {
+    return this;
+  }
 
   match(s1, s2) {
-    let isMatch = s1 === s2;
+    let s1prime = s1,
+      s2prime = s2;
+
+    this.rules.forEach(rule => {
+      assert(rule instanceof Map);
+      rule.forEach((v, k) => {
+        let _s1prime;
+        do {
+          _s1prime = s1prime;
+          s1prime = _s1prime.replace(k, v);
+        } while (_s1prime != s1prime);
+
+        let _s2prime;
+        do {
+          _s2prime = s2prime;
+          s2prime = _s2prime.replace(k, v);
+        } while (_s2prime != s2prime);
+      });
+    });
+
+    let isMatch = s1prime === s2prime;
     return { isMatch: isMatch };
   }
 
--- index.test.js
+++ index.test.js
@@ -4,7 +4,6 @@ describe('Rubric', () => {
   describe('language builtins', () => {
     it('should have en builtins', () => {
       expect(Rubric.en).toEqual(
-      const instance = new Rubric();
         expect.objectContaining({
           COMMON_PUNCTUATION: expect.stringMatching(/.*/),
           COMMON_SYMBOLS: expect.stringMatching(/.*/),
@@ -23,18 +22,38 @@ describe('Rubric', () => {
   });
 
   describe('match (default rules)', () => {
-    test('it should return false when inputs are not byte-equal', () => {
+    const instance = new Rubric();
+    it('should return false when inputs are not byte-equal', () => {
       const inputs = [['a', 'b']];
       inputs.forEach(([s1, s2]) => {
         expect(instance.match(s1, s2)).toEqual({ isMatch: false });
       });
     });
 
-    test('it should return true when inputs are byte-equal', () => {
+    it('should return true when inputs are byte-equal', () => {
       const inputs = [['a', 'a'], ['💩', '\u{1F4A9}']];
-      const instance = new Rubric();
       inputs.forEach(([s1, s2]) => {
         expect(instance.match(s1, s2)).toEqual({ isMatch: true });
+      });
+    });
+  });
+
+  describe('match (builtin doesnt matter)', () => {
+    const instance = new Rubric();
+    instance.doesntMatter(Rubric.en.COMMON_PUNCTUATION);
+
+    it("should return true when inputs differ solely by characters that don't matter", () => {
+      const inputs = [['what, you did', 'what you did']];
+      inputs.forEach(([s1, s2]) => {
+        expect(instance.match(s1, s2)).toEqual({ isMatch: true });
+      });
+    });
+
+    it('should return false when inputs differ by characters that do matter', () => {
+      const inputs = [['what he did', 'what you did']];
+      const instance = new Rubric();
+      inputs.forEach(([s1, s2]) => {
+        expect(instance.match(s1, s2)).toEqual({ isMatch: false });
       });
     });
   });
